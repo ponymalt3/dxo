485a846a4df5133f143cdd7d61d7a896640cc793
ponymalt3 <malte.graeper@cyno.tech>
2024-11-17 12:00:56 +0100

    Debug commit

--- alsa_plugin.cpp
+++ alsa_plugin.cpp
@@ -34,6 +34,7 @@ public:
     memset(this, 0, sizeof(snd_pcm_ioplug_t));
 
     auto coeffs = loadFIRCoeffs(path);
+    std::cout << "coeffs: " << (coeffs.size()) << std::endl;
     assert(coeffs.size() == 7 && "Coeffs file need to provide 7 FIR transfer functions");
 
     std::vector<FirMultiChannelCrossover::ConfigType> config{{0, coeffs[0]},
@@ -43,6 +44,8 @@ public:
                                                              {1, coeffs[4]},
                                                              {1, coeffs[5]},
                                                              {2, coeffs[6]}};
+
+    std::cout << "create Crossover: " << std::endl;
 
     crossover_ = std::make_unique<FirMultiChannelCrossover>(blockSize_, 3, config, 2);
 
@@ -55,6 +58,8 @@ public:
     {
       outputs_[i] = crossover_->getOutputBuffer(i).data();
     }
+
+    std::cout << "Plugin created" << std::endl;
   }
 
   ~AlsaPluginDxO() {}
@@ -109,12 +114,15 @@ public:
   template <bool _HasLfeChannel, typename _InputSampleType>
   uint32_t update(PcmStream<_InputSampleType>& src, uint32_t size)
   {
+    std::cout << "Update " << (size) << std::endl;
+
     PcmStream<int16_t> dst{outputBuffer_.get(), kNumOutputChannels};
 
     auto i{0U};
     while(i < size)
     {
       uint32_t segmentSize = std::min(size - i, blockSize_ - inputOffset_);
+      std::cout << " Segment " << (segmentSize) << std::endl;
 
       if(_HasLfeChannel)
       {
@@ -123,6 +131,8 @@ public:
       }
       else
       {
+        // std::cout << "  two channels" << std::endl;
+
         src.extractInterleaved(segmentSize, inputs_[0] + inputOffset_, inputs_[1] + inputOffset_);
 
         for(auto j{inputOffset_}; j < inputOffset_ + segmentSize; ++j)
@@ -169,8 +179,9 @@ public:
 
     if(result == -EPIPE)
     {
-      snd_output_printf(output_, "underflow\n");  // snd_strerror(result));
-      snd_output_flush(output_);
+      // snd_output_printf(output_, "underflow\n");  // snd_strerror(result));
+      // snd_output_flush(output_);
+      std::cout << "SyncOutputBuffer error: " << (snd_strerror(result)) << std::endl;
     }
 
     return result == blockSize_;
@@ -194,6 +205,8 @@ extern "C" {
 snd_pcm_sframes_t dxo_pointer(snd_pcm_ioplug_t* io)
 {
   auto* plugin = reinterpret_cast<AlsaPluginDxO*>(io);
+  std::cout << "dxo_pointer " << (plugin->streamPos_ % (plugin->buffer_size / 2)) << std::endl;
+
   return plugin->streamPos_ % (plugin->buffer_size / 2);
 }
 
@@ -234,6 +247,7 @@ snd_pcm_sframes_t dxo_transfer(snd_pcm_ioplug_t* ext,
 
 int dxo_prepare(snd_pcm_ioplug_t* ext)
 {
+  std::cout << "dxo_prepare" << std::endl;
   auto* plugin = reinterpret_cast<AlsaPluginDxO*>(ext);
 
   if(snd_pcm_open(&(plugin->pcm_), plugin->pcmName_.c_str(), SND_PCM_STREAM_PLAYBACK, 0) < 0)
@@ -260,6 +274,7 @@ int dxo_prepare(snd_pcm_ioplug_t* ext)
   }
 
   uint32_t rate = plugin->rate;
+  std::cout << "rate: " << (rate) << std::endl;
   if(snd_pcm_hw_params_set_rate_near(plugin->pcm_, plugin->params_, &rate, 0) < 0)
   {
     snd_output_printf(plugin->output_, "Can't set rate.");
@@ -369,6 +384,12 @@ SND_PCM_PLUGIN_DEFINE_FUNC(dxo)
   std::string slavePcm;
   snd_config_t* slaveConfig = nullptr;
 
+  snd_output_t* output;
+  snd_output_stdio_attach(&(output), stdout, 0);
+
+  snd_output_printf(output, "XXX");
+  snd_output_flush(output);
+
   snd_config_iterator_t i, next;
   snd_config_for_each(i, next, conf)
   {
@@ -424,8 +445,8 @@ SND_PCM_PLUGIN_DEFINE_FUNC(dxo)
     return -EINVAL;
   }
 
-  snd_output_t* output;
-  snd_output_stdio_attach(&(output), stdout, 0);
+  // snd_output_t* output;
+  // snd_output_stdio_attach(&(output), stdout, 0);
 
   AlsaPluginDxO* plugin = new AlsaPluginDxO(coeffPath, blockSize);
   plugin->callback = &callbacks;
@@ -436,6 +457,8 @@ SND_PCM_PLUGIN_DEFINE_FUNC(dxo)
   plugin->pcmName_ = slavePcm;
 
   int32_t result = snd_pcm_ioplug_create(plugin, name, stream, mode);
+
+  std::cout << "snd_pcm_ioplug_create " << (result) << std::endl;
 
   if(result < 0)
   {
