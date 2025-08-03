832824ac5e97bc58ccd6d315a1e9c651d56dab05
ponymalt3 <malte.graeper@cyno.tech>
2025-08-03 17:31:36 +0200

    Initialize in open function

--- alsa_plugin.h
+++ alsa_plugin.h
@@ -6,7 +6,6 @@
 #include <chrono>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -143,6 +142,12 @@ public:
         auto start = std::chrono::high_resolution_clock::now();
         memcpy(outputs_[0], inputs_[0], sizeof(float) * blockSize_);
         memcpy(outputs_[1], inputs_[1], sizeof(float) * blockSize_);
+        memcpy(outputs_[2], inputs_[0], sizeof(float) * blockSize_);
+        memcpy(outputs_[3], inputs_[1], sizeof(float) * blockSize_);
+        memcpy(outputs_[4], inputs_[0], sizeof(float) * blockSize_);
+        memcpy(outputs_[5], inputs_[1], sizeof(float) * blockSize_);
+        memcpy(outputs_[6], inputs_[2], sizeof(float) * blockSize_);
+
         // crossover_->updateInputs();
         auto end = std::chrono::high_resolution_clock::now();
 
@@ -183,7 +188,7 @@ public:
     return 0;
   }
 
-  uint32_t blockSize_{128};
+  uint32_t blockSize_{};
   std::vector<float*> inputs_{nullptr};
   std::vector<float*> outputs_{nullptr};
   uint32_t inputOffset_{0};
