e37150c0ce37893eefcd7847509b7d50fba45e9d
ponymalt3 <malte.graeper@cyno.tech>
2025-08-19 00:45:55 +0200

    Fix delay calculation

--- alsa_plugin.cpp
+++ alsa_plugin.cpp
@@ -130,6 +130,21 @@ snd_pcm_sframes_t AlsaPluginDxO::dxo_pointer(snd_pcm_ioplug_t* io)
 {
   auto* plugin = reinterpret_cast<AlsaPluginDxO*>(io);
   return plugin->streamPos_ % (plugin->buffer_size / 2);
+  /*
+    // Verfügbar-Update beim Slave erzwingen
+    if(snd_pcm_avail_update(plugin->pcm_output_device_) < 0)
+    {
+      return 0;
+    }
+
+    // Hardware-Pointer des Slave-Geräts ermitteln
+    snd_pcm_sframes_t delay;
+    snd_pcm_delay(plugin->pcm_output_device_, &delay);
+
+    // Unser hw_ptr aus appl_ptr und delay rekonstruieren
+    const auto hw_ptr = (io->appl_ptr + io->buffer_size - delay) % io->buffer_size;
+
+    return hw_ptr;*/
 }
 
 snd_pcm_sframes_t AlsaPluginDxO::dxo_transfer(snd_pcm_ioplug_t* io,
@@ -138,12 +153,6 @@ snd_pcm_sframes_t AlsaPluginDxO::dxo_transfer(snd_pcm_ioplug_t* io,
                                               snd_pcm_uframes_t size)
 {
   auto* plugin = reinterpret_cast<AlsaPluginDxO*>(io);
-
-  if(!plugin->pcm_output_device_)
-  {
-    plugin->print("Device not opened!");
-    return -EBUSY;
-  }
 
   const auto writer = [plugin](const int16_t* data, uint32_t frames) {
     return plugin->writePcm(data, frames);
@@ -242,6 +251,7 @@ int AlsaPluginDxO::dxo_prepare(snd_pcm_ioplug_t* io)
   plugin->print("dxo_prepare");
   plugin->streamPos_ = 0;
   plugin->inputOffset_ = 0;
+  plugin->crossover_->resetDelayLine();
   return 0;
 }
 
@@ -347,17 +357,21 @@ int AlsaPluginDxO::dxo_hw_params(snd_pcm_ioplug_t* io, snd_pcm_hw_params_t* para
 int AlsaPluginDxO::dxo_delay(snd_pcm_ioplug_t* io, snd_pcm_sframes_t* delayp)
 {
   auto* plugin = reinterpret_cast<AlsaPluginDxO*>(io);
-  plugin->print("dxo_delay");
+  // plugin->print("dxo_delay");
 
   snd_pcm_sframes_t slave_delay{0};
-  const auto result = snd_pcm_delay(plugin->pcm_output_device_, &slave_delay);
+  /*const auto result = snd_pcm_delay(plugin->pcm_output_device_, &slave_delay);
   if(result < 0)
   {
     plugin->print("snd_pcm_delay failed!");
-    return result;
-  }
+    return 0;
+  }*/
 
-  *delayp = slave_delay + plugin->inputOffset_;
+  const auto firDelay = 1;
+  const auto algorithmDelay = plugin->blockSize_ - plugin->inputOffset_;
+  *delayp = 0;  // firDelay + algorithmDelay;
+  // plugin->print("dxo_delay ", *delayp);
+
   return 0;
 }
 
@@ -474,7 +488,7 @@ SND_PCM_PLUGIN_DEFINE_FUNC(dxo)
     return -EINVAL;
   }
 
-  if(snd_pcm_ioplug_set_param_minmax(plugin, SND_PCM_IOPLUG_HW_PERIOD_BYTES, 16 * 1024, 2 * 1024 * 1024) < 0)
+  if(snd_pcm_ioplug_set_param_minmax(plugin, SND_PCM_IOPLUG_HW_PERIOD_BYTES, 1024, 2 * 1024 * 1024) < 0)
   {
     plugin->print("SND_PCM_IOPLUG_HW_PERIOD_BYTES failed");
     return -EINVAL;
