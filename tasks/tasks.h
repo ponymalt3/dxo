7d49b51400124a33ed8a6bbd3e9c434a17b012bb
ponymalt3 <malte.graeper@cyno.tech>
2026-08-23 10:52:10 +0200

    Make tasks more robust

--- tasks/tasks.h
+++ tasks/tasks.h
@@ -32,18 +32,17 @@ class Task : public std::enable_shared_from_this<Task>, public ThreadSafeList::N
 public:
   void execute(const std::function<void(std::shared_ptr<Task>)>& dep_resolved)
   {
+    dependenciesLeft_.store(dependencies_.size(), std::memory_order_relaxed);
+
     callback_(*this);  // Execute the task
 
     for(auto& d : dependents_)
     {
-      if(--(d->dependenciesLeft_) == 0)
+      if(d->dependenciesLeft_.fetch_sub(1, std::memory_order_acq_rel) == 1)
       {
         dep_resolved(d);
       }
     }
-
-    // reset dependencies count
-    dependenciesLeft_ = dependencies_.size();
   }
 
   const std::vector<std::shared_ptr<Task>>& getDependencies() const { return dependencies_; }
@@ -166,7 +165,11 @@ public:
 protected:
   void restartWorker()
   {
-    ++epoch_;
+    {
+      std::lock_guard lock(mutex_);
+      ++epoch_;
+    }
+
     cv_.notify_all();
   }
 
